a948831404029df4991a3e10945a57b2e48b6c5a
mwaqasafzal <mwaqas7064@gmail.com>
2020-07-27 23:28:00 +0500

    improved referential integrety

--- app.py
+++ app.py
@@ -81,8 +81,8 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id',ondelete="cascade"), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id',ondelete="cascade"), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
